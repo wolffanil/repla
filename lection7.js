1cb6d278a5605f8395184de576ce6236768b624d
Fianil Khasanov <91341349+wolffanil@users.noreply.github.com>
2023-10-25 13:56:03 +0400

    Add files via upload

---
+++ lection7.js
@@ -0,0 +1,158 @@
+
+//Задание 1
+// let str = 'aba aca aea abba adca abea';
+// console.log(str.replace(/a..a/g,''));
+//Задание 2
+// let str = 'aa aba abba abbba abca abea';
+// let res = str.replace(/ab*a/g, '!');
+// console.log(res);
+//Задание 3
+// let str = 'ab abab abab abababab abea';
+// let res = str.replace(/(ab)/g, '!');
+// console.log(res);
+//Задание 4
+// let str = 'a.a aba aea';
+// let res = str.replace(/a\.a/g, '!');
+// console.log(res);
+//Задание 5
+// let str = 'aa aba abba abbba abbbba abbbbba';
+// let res = str.replace(/ab{1,3}a/g, '!');
+// console.log(res);
+//Задание 6
+// let str = 'aba accca azzza wwwwa';
+// let res = str.replace(/a.+?a/g, '!');
+// console.log(res);
+//Задание 7
+// let str = 'a1a a22a a333a a4444a a55555a aba aca';
+// let res = str.replace(/a\d+?a/g, '!');
+// console.log(res);
+//Задание 8
+// let str = 'a1a az3a a7a a9a aba';
+// let res = str.replace(/a[3-6]*a/g, '!');
+// console.log(res);
+//Задание 9
+// let str = 'aAXa aeffa aGha aza ax23a a3sSa';
+// let res = str.replace(/a[a-z0-9]+a/g, '!');
+// console.log(res)
+//Задание 10
+// let str = 'x2z x9z';
+// let res = str.replace(/x[^2-7]z/g, '!');
+// console.log(res)
+
+//Задание 11
+// let str = 'xz xaZ4z';
+// let res = str.replace(/x[^a-zA-Z1-5]*z/g, '!')
+// console.log(res)
+
+//Задание 12
+// let str = 'wйw wяw wёw wqw';
+// let res = str.replace(/w[а-яё]*w/g, '!');
+// console.log(res);
+
+
+//Задание 13
+
+// let str = 'xaz x.z x3z x@z x$z xrz';
+// let res = str.replace(/x[^.@$]z/g, '!');
+// console.log(res);
+
+//Задание 14
+// let str = '1.23 4 56.7 8.9.0 ac';
+// let res = str.replace(/[\d.]+/g, "!");
+// console.log(res);
+
+//Задание 15
+// let str = 'x[]z x[[]]z x()z';
+// let res = str.replace(/x\[[^\]]*\]z/g, '!');
+// console.log(res);
+
+//Задание 16
+// let str = '^xx axx ^zz bkk @ss';
+// let res = str.replace(/[\^@][a-zA-Z]{2}/g, '!');
+// console.log(res);
+
+//Задание 17
+// let str = 'xaz xBz xcz x-z x@z';
+// let res = str.replace(/x[a-zA-Z-]z/g, "!");
+// console.log(res);
+
+//Задание 18
+// let str = 'abc def xyz';
+// let res =str.replace(/[a-zA-Z]+/, "!");
+// console.log(res)
+
+//Задание 19
+let str = `abc de fghi jkl`;
+
+let res = str.replace(/(^|\n)/g, '!$1');
+
+
+console.log(res);
+
+//Задание 20
+// console.log(res)
+
+//Задание 21
+// console.log(res)
+
+
+//Задание 22
+// console.log(res)
+//Задание 23
+// console.log(res)
+
+//Задание 24
+// console.log(res)
+
+//Задание 25
+// console.log(res)
+
+//Задание 26
+// console.log(res)
+
+//Задание 27
+// console.log(res)
+
+//Задание 28
+// console.log(res)
+
+//Задание 29
+
+// console.log(res)
+
+//Задание 30
+// console.log(res)
+
+//Задание 31
+// console.log(res)
+
+//Задание 32
+// console.log(res)
+
+//Задание 33
+// console.log(res)
+
+//Задание 34
+// console.log(res)
+
+//Задание 35
+// console.log(res)
+
+//Задание 36
+// console.log(res)
+
+//Задание 37
+// console.log(res)
+
+//Задание 38
+// console.log(res)
+
+//Задание 39
+// console.log(res)
+
+
+
+
+
+
+
